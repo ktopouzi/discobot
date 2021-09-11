8dfccfdd66cb140215490a235f8f85148b0ee3fc
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2021-09-11 22:34:35 +0100

    fixed music command

--- commands/play.js
+++ commands/play.js
@@ -1,20 +1,22 @@
 const ytdl = require('ytdl-core');
 
-module.exports = message => {
-    const url = message.content.split(" ")[1];
-    console.log(url);
-    if (message.channel.type !== 'text') return;
+module.exports = (message) => {
+  const url = message.content.split(' ')[1];
+  if (message.channel.type !== 'text') return;
 
-    const voiceChannel = message.member.voice.channel;
+  const voiceChannel = message.member.voice.channel;
 
-    if (!voiceChannel) {
-        return message.reply('Please join a voice channel first!');
-    }
+  if (!voiceChannel) {
+    return message.reply('Please join a voice channel first!');
+  }
 
-    voiceChannel.join().then(connection => {
-        const stream = ytdl(url ? url : 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ', { filter: 'audioonly' });
-        const dispatcher = connection.play(stream);
-        dispatcher.setVolume(0.25);
-        dispatcher.on('end', () => voiceChannel.leave());
-    });
-}
+  voiceChannel.join().then((connection) => {
+    const stream = ytdl(
+      url ? url : 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ',
+      { filter: 'audioonly' }
+    );
+    const dispatcher = connection.play(stream);
+    dispatcher.setVolume(0.25);
+    dispatcher.on('end', () => voiceChannel.leave());
+  });
+};
